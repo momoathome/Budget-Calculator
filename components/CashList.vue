67c0b41f00d7b47723be75cbf2be45bcb1b67aea
Maurice Knoll <maurice.knoll@adesso.de>
2023-07-26 15:36:25 +0200

    [feat] added experimental defineModel

--- components/CashList.vue
+++ components/CashList.vue
@@ -9,6 +9,8 @@ const props = defineProps<{
 }>()
 
 const description = props.index === 'Income' ? 'Income' : 'Expense'
+
+const inputValue = ref('')
 </script>
 
 <template>
@@ -18,7 +20,7 @@ const description = props.index === 'Income' ? 'Income' : 'Expense'
     </h4>
     <ul class="my-2 flex flex-col gap-4 ps-0">
       <cash-item v-for="(item) in props.data" :key="item.id" :item="item" />
-      <new-cash-item :key="index" :description="description" />
+      <new-cash-item :key="index" v-model="inputValue" :description="description" />
     </ul>
   </div>
 </template>
--- components/NewCashItem.vue
+++ components/NewCashItem.vue
@@ -3,17 +3,24 @@ defineProps<{
   description: string
 }>()
 
-const inputText = ref('')
+// Vue Macros
+/* const { modelValue } = defineModels<{
+  modelValue: string
+}>()
 
-// @keydown.enter.exact.prevent="submit()"
+function onInput(e) {
+  modelValue.value = e.target.value
+} */
+
+// Vue 3.3 Experimental
+const modelValue = defineModel()
 </script>
 
 <template>
   <form action="" @submit.prevent>
     <li class="relative flex list-none items-center gap-2 bg-base text-xl font-600 shadow-md text-primary">
-      <input v-model="inputText"
-        class="w-full border-none bg-base px-4 py-3 text-xl font-600 outline-3 outline-primary text-base_dark hover:cursor-pointer focus-visible:cursor-text placeholder-font-600 focus-visible:outline-solid hover:outline-solid placeholder-text-primary focus-visible:placeholder-text-primary_light"
-        type="text" :placeholder="`new ${description}`">
+      <input v-model="modelValue" type="text" :placeholder="`new ${description}`"
+        class="w-full border-none bg-base px-4 py-3 text-xl font-600 outline-3 outline-primary text-base_dark hover:cursor-pointer focus-visible:cursor-text placeholder-font-600 focus-visible:outline-solid hover:outline-solid placeholder-text-primary focus-visible:placeholder-text-primary_light">
       <button type="submit" class="absolute right-4 top-3.5 text-2xl" prevent="default" i-tabler-circle-plus />
     </li>
   </form>
