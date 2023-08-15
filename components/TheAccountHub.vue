8e73efe686c9a1baeabba2d819131f7af79df17a
Maurice Knoll <maurice.knoll@adesso.de>
2023-08-15 15:34:47 +0200

    [feat] added transition to Account Hub

--- components/TheAccountHub.vue
+++ components/TheAccountHub.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
+const isMenuShown = ref(false)
 const target = ref(null)
 const ignoreElRef = ref(null)
-const isMenuShown = ref(false)
 onClickOutside(target, () => isMenuShown.value = false, { ignore: [ignoreElRef] })
 </script>
 
@@ -12,9 +12,22 @@ onClickOutside(target, () => isMenuShown.value = false, { ignore: [ignoreElRef] 
     <p for="languageListBox" class="m-0 text-sm font-medium text-dark-900/60 md:hidden dark:text-[rgba(235,235,235,.6)]">
       Account
     </p>
-    <div ref="ignoreElRef" i="tabler-user-circle" class="cursor-pointer bg-base_dark text-7 icon-btn" @click="isMenuShown = !isMenuShown" />
+    <button ref="ignoreElRef" type="button" i="tabler-user-circle" class="cursor-pointer bg-base_dark text-7 icon-btn" @click="isMenuShown = !isMenuShown" />
 
-    <!-- On Hover over Icon soll SignIn Component erscheinen -->
-    <Logout v-show="isMenuShown" ref="target" class="absolute right--4 top-16" />
+    <Transition>
+      <Logout v-show="isMenuShown" ref="target" class="absolute right--4 top-16" />
+    </Transition>
   </div>
 </template>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.2s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
