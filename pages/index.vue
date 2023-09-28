46aaf2401188c0f02cc12c9c6db2e9b609fdfa9e
Maurice Knoll <maurice.knoll@adesso.de>
2023-09-28 11:02:15 +0200

    [feat] bugfix for overflow on Landing Page

--- pages/index.vue
+++ pages/index.vue
@@ -6,14 +6,14 @@ const showGetStartedButton = ref(true)
 </script>
 
 <template>
-  <the-navbar />
+  <div class="h-100dvh overflow-hidden bg-white dark:bg-base" :class="{ 'h-[calc(100dvh+230px)]!': showRegister, 'h-[calc(100dvh+200px)]!': showLogin }">
+    <the-navbar />
 
-  <div class="h-[calc(100dvh-70px)] bg-white dark:bg-base">
     <div class="relative isolate px-6 pt-14 lg:px-8">
       <div class="absolute inset-x-0 transform-gpu overflow-hidden blur-3xl -top-40 -z-10 sm:-top-80" aria-hidden="true">
         <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] rotate-[30deg] from-[#ff80b5] to-[#9089fc] bg-gradient-to-tr opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem] -translate-x-1/2" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
       </div>
-      <div class="mx-auto max-w-2xl py-32 lg:py-56 sm:py-48">
+      <div class="mx-auto max-w-2xl lg:py-56 sm:py-32">
         <div class="text-center">
           <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl dark:text-gray-200">
             Budget Rechner
@@ -29,7 +29,7 @@ const showGetStartedButton = ref(true)
         </div>
 
         <div class="relative flex justify-center">
-          <div class="absolute mt-10">
+          <div class="absolute lg:mt-10">
             <TransitionGroup name="test">
               <Login v-if="!authUser && showLogin" @show-register="showLogin = false, showRegister = true" />
               <TheRegister v-if="!authUser && showRegister" @show-login="showRegister = false, showLogin = true" />
